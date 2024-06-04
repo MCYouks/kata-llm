60938dd922c6b042e4e57b0ece8e01d2321fdf5b
@mcyouks <andreas.blondeau@gmail.com>
2024-06-04 14:14:01 +0200

    Update app.vue

--- app.vue
+++ app.vue
@@ -9,37 +9,6 @@
 </template>
 
 <script setup lang="ts">
-import { resolveStream } from "~/utils/chat-stream";
-
-const question = ref("What is the concept of relativity?")
-const answer = ref("")
-const loading = ref(false)
-
-const getAnswer = async function () {
-  try {
-    loading.value = true;
-    answer.value = "";
-
-    /**
-     * Note: we are using the native `fetch` API to handle readable streams.
-     */
-    const { body: stream } = await fetch("/api/generate-stream-response", { 
-      method: "POST", 
-      body: JSON.stringify({ question: question.value }) 
-    });
-
-    if (!stream) throw new Error("No stream returned!");
-
-    resolveStream(stream, {
-      onChunk: (content) => {
-        answer.value += content;
-      },
-    });
-  } catch (error) {
-  } finally {
-    loading.value = false;
-  }
-}
 </script>
 
 <style scoped lang="scss">
